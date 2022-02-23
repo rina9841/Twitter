e12dddc9324c2af622d4b69740a5e7f49df5bbe7
rina9841 <pandyrina@gmail.com>
2022-02-22 22:26:01 -0500

    adding maven project along with pom.xml and java files

---
+++ src/main/java/com/Twitter/Twitter/TwitterApplication.java
@@ -0,0 +1,13 @@
+package com.Twitter.Twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApplication.class, args);
+	}
+
+}
